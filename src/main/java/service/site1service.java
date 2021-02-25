8e6f58c437b15167a058cc1b7c5b76aedf00af33
zxlz <xueliang.zhu123@qq.com>
2021-02-25 19:22:18 +0800

    1

--- src/main/java/service/site1service.java
+++ src/main/java/service/site1service.java
@@ -6,26 +6,39 @@ import redis.clients.jedis.JedisPool;
 import spiderpak.service.BaseService;
 import spiderpak.utils.Log;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.Set;
 
 
 public class site1service extends BaseService {
     private static JedisPool jedisPool=null;
-    private static void initCon() throws ClassNotFoundException, InterruptedException {
+    private static void initCon() throws  InterruptedException, IOException {
+        String f = "/jedis.properties";
+        Properties props = new Properties();
+//        props.load(new java.io.FileInputStream(f));
+        props.load(site1service.class.getResourceAsStream("/jedis.properties"));
+        long maxWaitMillis= Long.parseLong(props.getProperty("maxWaitMillis"));
+        String host=props.getProperty("host");
+        int port= Integer.parseInt(props.getProperty("port"));
+        int timeout= Integer.parseInt(props.getProperty("timeout"));
+        String password=props.getProperty("password");
+
 
         int trycount=0;
         boolean ready=false;
         while(!ready) {
             try {
                 GenericObjectPoolConfig config=new GenericObjectPoolConfig();
-                config.setMaxWaitMillis(2000);
-                jedisPool = new JedisPool(config,"192.168.0.3",6379,10000,"zxcvbnmmnbvcxz");
+                config.setMaxWaitMillis(maxWaitMillis);
+                jedisPool = new JedisPool(config,host,port,timeout,password);
 
                 ready=true;
             }catch (Exception e){
                 //连接失败，阻塞重试
-                Log.severe(("error: "+ ++trycount+""+e.getMessage()));
+                Log.warning(("error: "+ ++trycount+""+e.getMessage()));
                 Thread.sleep(10000);
             }
         }
@@ -73,7 +86,7 @@ public class site1service extends BaseService {
         //获取已存代理
         Jedis jedis = jedisPool.getResource();
         jedis.select(1);
-        Set<String> set=jedis.hkeys("allproxy");
+        Set<String> set=jedis.hkeys("proxy_all");
         jedis.close();
         for (String s:set) {
             task.getRepeatFilter().add(s);
